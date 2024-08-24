1f2319fe03c9112e01a4a293ee05389d71b3c181
remreim <studen_studen@yahoo.com>
2024-08-24 22:55:13 +0300

    app side init

---
+++ Basic Project Generator/Basic Project Generator/Interfaces/ApiResolver.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Security.AccessControl;
+
+namespace Basic_Project_Generator.Interfaces
+{
+    public static class ApiResolver
+    {
+        #region constants
+
+        public const string Version = "V17";
+        private const string LibraryKey = "SOFTWARE\\Siemens\\Automation\\Openness\\17.0\\PublicAPI\\17.0.0.0";
+        private const string LibraryName = "Siemens.Engineering";
+
+        #endregion // constants
+
+        #region methods
+
+        /// <summary>
+        /// Determines the API library to be loaded 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static Assembly AssemblyResolver(object sender, ResolveEventArgs args)
+        {
+            var lookupName = new AssemblyName(args.Name);
+            if (lookupName.Name.Equals(LibraryName, StringComparison.OrdinalIgnoreCase))
+            {
+                var libraryFilePath = GetLibraryFilePath();
+                if (!string.IsNullOrWhiteSpace(libraryFilePath))
+                {
+                    var suggestedName = AssemblyName.GetAssemblyName(libraryFilePath);
+                    return Assembly.Load(suggestedName);
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Determines if the version of the API library is installed
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsOpennessInstalled()
+        {
+            return !string.IsNullOrWhiteSpace(GetLibraryFilePath());
+        }
+
+        private static string GetLibraryFilePath()
+        {
+            using (var baseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+            {
+                using (var registryKey = baseKey.OpenSubKey(LibraryKey, RegistryKeyPermissionCheck.ReadSubTree, RegistryRights.ReadKey))
+                {
+                    var libraryFilePath = registryKey?.GetValue(LibraryName) as string;
+                    if (!string.IsNullOrWhiteSpace(libraryFilePath) && File.Exists(libraryFilePath))
+                    {
+                        return libraryFilePath;
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ Basic Project Generator/Basic Project Generator/Program.cs
@@ -0,0 +1,35 @@
+﻿using Basic_Project_Generator.Interfaces;
+using Basic_Project_Generator.UserInterfaces;
+using System;
+using System.Windows.Forms;
+
+namespace Basic_Project_Generator
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+
+                if (!ApiResolver.IsOpennessInstalled())
+                {
+                    throw new Exception($"TIA Portal Openness {ApiResolver.Version} is not installed.");
+                }
+                AppDomain.CurrentDomain.AssemblyResolve += ApiResolver.AssemblyResolver;
+
+                Application.Run(new BasicProjectGenerator());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}{Environment.NewLine}{Environment.NewLine}Details:{Environment.NewLine}{ex}", "Basic Project Generator", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
